9e1994ee176c4016f309e6426eec5c224ddd4067
Ed Halliwell <edhalliwell@hotmail.com>
2021-10-13 09:41:21 +0100

    Added wordCount solution

--- src/skeleton.test.ts
+++ src/skeleton.test.ts
@@ -1,5 +1,11 @@
-import sum from "./skeleton";
+import { sum, wordCount } from "./skeleton";
 
 test("sum adds two numbers", () => {
   expect(sum(4, 3)).toBe(7);
 });
+
+test("count the number of words, minus the exceptions", () => {
+  expect(wordCount("Hello there, little user5453 374 ())$.")).toBe(4);
+  expect(wordCount("I’d been using my sphere as a stool. I traced counterclockwise circles on it with my fingertips and it shrank until I could palm it. My bolt had shifted while I’d been sitting. I pulled it up and yanked the pleats straight as I careered around tables, chairs, globes, and slow-moving fraas. I passed under a stone arch into the Scriptorium. The place smelled richly of ink. Maybe it was because an ancient fraa and his two fids were copying out books there. But I wondered how long it would take to stop smelling that way if no one ever used it at all; a lot of ink had been spent there, and the wet smell of it must be deep into everything.")).toBe(112);
+  expect(wordCount("a the on at of upon in as")).toBe(0);
+})
--- src/skeleton.ts
+++ src/skeleton.ts
@@ -4,8 +4,28 @@
  * @param b - the second number to add
  * @returns the total
  */
-function sum(a: number, b: number): number {
+export function sum(a: number, b: number): number {
   return a + b;
 }
 
-export default sum;
+/**
+ * Takes a string and counts the number of words, minus the exceptions given.
+ * @param inputString - the provided string / sentence
+ * @returns the word count
+ */
+export function wordCount(inputString: string): number {
+  let count = 0;
+  const exclusions = ["a", "the", "on", "at", "of", "upon", "in", "as"];
+  let stringSplit = inputString.replace(/([^A-Za-z\s])/g, ' ').replace(/\s\s+/g, ' ').trim().toLowerCase().split(' ')
+  for(let string of stringSplit) {
+    if(!exclusions.includes(string.toLowerCase())) count++;
+  }
+  return count;
+}
+
+
+
+
+// Write a test for the next bit of functionality you want to add.
+// Write the functional code until the test passes.
+// Refactor both new and old code to make it well structured.
