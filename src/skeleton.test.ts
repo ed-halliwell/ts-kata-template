97055b35c07dcd468b3535a8ac614738ce624728
Ed Halliwell <edhalliwell@hotmail.com>
2021-10-13 15:06:43 +0100

    Added solution for spin words with and without map

--- src/skeleton.test.ts
+++ src/skeleton.test.ts
@@ -1,4 +1,4 @@
-import { sum, wordCount, likes } from "./skeleton";
+import { sum, wordCount, likes, spinWords, spinWordsWithMap } from "./skeleton";
 
 test("sum adds two numbers", () => {
   expect(sum(4, 3)).toBe(7);
@@ -6,14 +6,36 @@ test("sum adds two numbers", () => {
 
 test("count the number of words, minus the exceptions", () => {
   expect(wordCount("Hello there, little user5453 374 ())$.")).toBe(4);
-  expect(wordCount("I’d been using my sphere as a stool. I traced counterclockwise circles on it with my fingertips and it shrank until I could palm it. My bolt had shifted while I’d been sitting. I pulled it up and yanked the pleats straight as I careered around tables, chairs, globes, and slow-moving fraas. I passed under a stone arch into the Scriptorium. The place smelled richly of ink. Maybe it was because an ancient fraa and his two fids were copying out books there. But I wondered how long it would take to stop smelling that way if no one ever used it at all; a lot of ink had been spent there, and the wet smell of it must be deep into everything.")).toBe(112);
+  expect(
+    wordCount(
+      "I’d been using my sphere as a stool. I traced counterclockwise circles on it with my fingertips and it shrank until I could palm it. My bolt had shifted while I’d been sitting. I pulled it up and yanked the pleats straight as I careered around tables, chairs, globes, and slow-moving fraas. I passed under a stone arch into the Scriptorium. The place smelled richly of ink. Maybe it was because an ancient fraa and his two fids were copying out books there. But I wondered how long it would take to stop smelling that way if no one ever used it at all; a lot of ink had been spent there, and the wet smell of it must be deep into everything.",
+    ),
+  ).toBe(112);
   expect(wordCount("a the on at of upon in as")).toBe(0);
-})
+});
 
-test("Return 'like' text output for given number of names", ()=> {
+test("Return 'like' text output for given number of names", () => {
   expect(likes([]).match("No one likes this"));
-  expect(likes(['Peter']).match('Peter likes this'));
-  expect(likes(['Jacob', 'Alex']).match('Jacob and Alex like this'));
-  expect(likes(['Max', 'John', 'Mark']).match('Max, John and Mark like this'));
-  expect(likes(['Alex', 'Jacob', 'Mark', 'Max']).match('Alex, Jacob and 2 others like this'));
-})
+  expect(likes(["Peter"]).match("Peter likes this"));
+  expect(likes(["Jacob", "Alex"]).match("Jacob and Alex like this"));
+  expect(likes(["Max", "John", "Mark"]).match("Max, John and Mark like this"));
+  expect(
+    likes(["Alex", "Jacob", "Mark", "Max"]).match(
+      "Alex, Jacob and 2 others like this",
+    ),
+  );
+});
+
+test("Returns a sentence with words over 5 characters reversed.", () => {
+  expect(spinWords("Hey fellow warriors").match("Hey wollef sroirraw"));
+  expect(spinWords("This is a test").match("This is a test"));
+  expect(spinWords("This is another test").match("This is rehtona test"));
+});
+
+test("Returns a sentence with words over 5 characters reversed.", () => {
+  expect(spinWordsWithMap("Hey fellow warriors").match("Hey wollef sroirraw"));
+  expect(spinWordsWithMap("This is a test").match("This is a test"));
+  expect(
+    spinWordsWithMap("This is another test").match("This is rehtona test"),
+  );
+});
--- src/skeleton.ts
+++ src/skeleton.ts
@@ -16,22 +16,66 @@ export function sum(a: number, b: number): number {
 export function wordCount(inputString: string): number {
   let count = 0;
   const exclusions = ["a", "the", "on", "at", "of", "upon", "in", "as"];
-  let stringSplit = inputString.replace(/([^A-Za-z\s])/g, ' ').replace(/\s\s+/g, ' ').trim().toLowerCase().split(' ')
-  for(let string of stringSplit) {
-    if(!exclusions.includes(string.toLowerCase())) count++;
+  let stringSplit = inputString
+    .replace(/([^A-Za-z\s])/g, " ")
+    .replace(/\s\s+/g, " ")
+    .trim()
+    .toLowerCase()
+    .split(" ");
+  for (let string of stringSplit) {
+    if (!exclusions.includes(string.toLowerCase())) count++;
   }
   return count;
 }
 
+/**
+ * Takes an array of names and outputs a 'like' sentence.
+ * @param names: string[]  - the provided list of names
+ * @returns a string, like 'X likes this' or 'X and Y like this'
+ */
 export function likes(names: string[]): string {
-  if(names.length === 1) return `${names[0]} likes this`;
-  if(names.length === 2) return `${names[0]} and ${names[1]} like this`;
-  if(names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-  if(names.length > 3) return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  if (names.length === 1) return `${names[0]} likes this`;
+  if (names.length === 2) return `${names[0]} and ${names[1]} like this`;
+  if (names.length === 3)
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  if (names.length > 3)
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
   else return "No one likes this";
 }
 
+/**
+ * Takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed
+ * @param inputString: string - the provided string
+ * @returns a string with words over 5 characters reversed
+ */
+export function spinWords(inputString: string): string {
+  let outputWords = [];
+  let splitWords = inputString.split(" ");
+  for (let word of splitWords) {
+    if (word.length >= 5) {
+      outputWords.push(word.split("").reverse().join(""));
+    } else {
+      outputWords.push(word);
+    }
+  }
+  return outputWords.join(" ").trim();
+}
 
+/**
+ * Solution uses map. Takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed
+ * @param inputString: string - the provided string
+ * @returns a string with words over 5 characters reversed
+ */
+export function spinWordsWithMap(inputString: string): string {
+  let outputWords: string[] = [];
+  let splitWords: string[] = inputString.split(" ");
+  splitWords.map((word: string) => {
+    word.length >= 5
+      ? outputWords.push(word.split("").reverse().join(""))
+      : outputWords.push(word);
+  });
+  return outputWords.join(" ").trim();
+}
 
 // Write a test for the next bit of functionality you want to add.
 // Write the functional code until the test passes.
