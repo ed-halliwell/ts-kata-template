da5850ba525f37c8de9f56ca69ec964627f107ab
Ed Halliwell <edhalliwell@hotmail.com>
2021-10-13 14:23:59 +0100

    Added likes solution and tests

--- src/skeleton.test.ts
+++ src/skeleton.test.ts
@@ -1,4 +1,4 @@
-import { sum, wordCount } from "./skeleton";
+import { sum, wordCount, likes } from "./skeleton";
 
 test("sum adds two numbers", () => {
   expect(sum(4, 3)).toBe(7);
@@ -8,4 +8,12 @@ test("count the number of words, minus the exceptions", () => {
   expect(wordCount("Hello there, little user5453 374 ())$.")).toBe(4);
   expect(wordCount("I’d been using my sphere as a stool. I traced counterclockwise circles on it with my fingertips and it shrank until I could palm it. My bolt had shifted while I’d been sitting. I pulled it up and yanked the pleats straight as I careered around tables, chairs, globes, and slow-moving fraas. I passed under a stone arch into the Scriptorium. The place smelled richly of ink. Maybe it was because an ancient fraa and his two fids were copying out books there. But I wondered how long it would take to stop smelling that way if no one ever used it at all; a lot of ink had been spent there, and the wet smell of it must be deep into everything.")).toBe(112);
   expect(wordCount("a the on at of upon in as")).toBe(0);
+})
+
+test("Return 'like' text output for given number of names", ()=> {
+  expect(likes([]).match("No one likes this"));
+  expect(likes(['Peter']).match('Peter likes this'));
+  expect(likes(['Jacob', 'Alex']).match('Jacob and Alex like this'));
+  expect(likes(['Max', 'John', 'Mark']).match('Max, John and Mark like this'));
+  expect(likes(['Alex', 'Jacob', 'Mark', 'Max']).match('Alex, Jacob and 2 others like this'));
 })
--- src/skeleton.ts
+++ src/skeleton.ts
@@ -23,6 +23,13 @@ export function wordCount(inputString: string): number {
   return count;
 }
 
+export function likes(names: string[]): string {
+  if(names.length === 1) return `${names[0]} likes this`;
+  if(names.length === 2) return `${names[0]} and ${names[1]} like this`;
+  if(names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  if(names.length > 3) return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  else return "No one likes this";
+}
 
 
 
